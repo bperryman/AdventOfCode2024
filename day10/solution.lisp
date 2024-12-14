9aff69596c59b8c8883a7961fa22d9dbf5fdf7e9
Barry Perryman <barry.perryman@gmail.com>
2024-12-14 09:11:08 +0000

    Day 10 - fixed

--- day10/solution.lisp
+++ day10/solution.lisp
@@ -1,27 +1,52 @@
 (defun load-data (file)
-  (aoc:process-dataset-to-matrix (aoc:read-data-file file) :element-type 'character))
-
-(defvar *trail-path* (list #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9))
+  (flet ((to-nums (str)
+           (map 'list #'(lambda (c) (- (char-code c) (char-code #\0))) str)))
+    (aoc:process-dataset-to-matrix (aoc:read-data-file file :line-processor #'to-nums) :element-type 'fixnum)))
 
 (defun trail-head-locations (data)
   "Return a collection of positions for each trail head in the data."
-  (aoc:positions-for data #\0 :test #'char=))
+  (aoc:positions-for data 0 :test #'=))
 
-(defun trails-from (pt data path)
-  (declare (optimize (debug 3)))
-  (labels ((walk-path (pt remaining-path)
-             (cond
-               ((null remaining-path) (list pt))
-               ((not (aoc:dataset-contains-point-p data pt)) ())
-               ((char= (aoc:data-at data pt) (first remaining-path))
-                (apply #'append (mapcar #'(lambda (pt)
-                                            (walk-path pt (rest remaining-path)))
-                                         (aoc:surrounding-points pt))))
-               (t nil))))
-    (walk-path pt path)))
+
+(defun trails-from (pt data)
+  "Returns a list of the end point of the trails that start from pt"
+  (let ((ends ()))
+    (labels ((walk (step expected)
+               ; (format t "walk ~a ~a~%" step expected)
+               (cond
+                 ((or (not (aoc:dataset-contains-point-p data step))
+                      (not (= (aoc:data-at data step) expected)))
+                  ())
+                 ((= expected 9 (aoc:data-at data step))
+                  (setf ends (adjoin step ends :test #'equalp)))
+                 (t (dolist (next-step (aoc:surrounding-points step))
+                      (walk next-step (1+ expected)))))))
+      (walk pt 0)
+      ends)))
+
 
 (defun solution-1 (file)
   (let ((data (load-data file)))
     (loop for head in (trail-head-locations data)
-          collect (trails-from head data *trail-path*) into all-trails
-          finally (return (remove-duplicates (apply #'append all-trails) :test #'equalp)))))
+          sum (length (trails-from head data)))))
+
+(defun count-trails-from (pt data)
+  "Returns a list of the end point of the trails that start from pt"
+  (let ((count 0))
+    (labels ((walk (step expected)
+               ; (format t "walk ~a ~a~%" step expected)
+               (cond
+                 ((or (not (aoc:dataset-contains-point-p data step))
+                      (not (= (aoc:data-at data step) expected)))
+                  ())
+                 ((= expected 9 (aoc:data-at data step))
+                  (incf count))
+                 (t (dolist (next-step (aoc:surrounding-points step))
+                      (walk next-step (1+ expected)))))))
+      (walk pt 0)
+      count)))
+
+(defun solution-2 (file)
+  (let ((data (load-data file)))
+    (loop for head in (trail-head-locations data)
+          sum (count-trails-from head data))))
